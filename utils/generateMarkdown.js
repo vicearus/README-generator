7b0a3c2b57f62a89b2b30c628342535dde261db8
vicearus <ajvicenc@uci.edu>
2023-01-13 22:43:10 -0800

    added functions regarding license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,17 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = license =>
+  license === "NONE" ? "" : `<br>![${license} License Badge](https://img.shields.io/badge/license-${license}-blue.svg)`;
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = license =>
+  license === "NONE" ? "" : `<br>[License](#License) <br>`;
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseSection = license =>
+  license === "NONE" ? "" :
+    `## License
+    License: ${license}.`;
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = data =>
@@ -19,14 +22,13 @@ const generateMarkdown = data =>
   [Installation](#Installation) <br>
   [Test](#Test) <br>
   [Usage](#Usage) <br>
-  [Credits](#Credits)
+  [Credits](#Credits)${renderLicenseLink(data.license)}
   [Questions](#Questions) <br>
   ## Installation
   Install dependencies by running the following command in terminal: 
   \`\`\`
   ${data.dependency}
   \`\`\`
-  Access the webpage via the following link: [${data.url}](${data.url})
   ## Test
   Perform tests by running the following command in terminal: 
   \`\`\`
@@ -36,6 +38,7 @@ const generateMarkdown = data =>
   ${data.usage}
   ## Credits
   ${data.credits}
+  ${renderLicenseSection(data.license)}
   ## Questions
   For questions, please email me at ${data.email}
   For other projects, please check out my GitHub at [github.com/${data.username}](github.com/${data.username})
